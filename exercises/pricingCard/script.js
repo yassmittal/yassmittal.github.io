7307576e0a8e119e27e12044336ec34e57c5cf4c
yassmittal <yashmittalmm@gmail.com>
2022-11-12 17:39:39 +0530

    add script file to pricing card project

---
+++ exercises/pricingCard/script.js
@@ -0,0 +1,33 @@
+console.log("Hello world");
+const basic_plan_price = document.querySelector('#basic-price-plan');
+const master_plan_price = document.querySelector("#master-price-plan")
+const professional_plan_price = document.querySelector('#professional-price-plan');
+const checkbox = document.querySelector('#checkbox');
+
+checkbox.addEventListener("click", function(){
+  console.log("click")
+ let basic_plan = basic_plan_price.textContent;
+ let master_plan = master_plan_price.textContent;
+ let professional_plan = professional_plan_price.textContent;
+ master_plan_price.textContent = master_plan === '$399.99' ? '$39.99' : '$399.99';
+ professional_plan_price.textContent = professional_plan === '$249.99' ? '$24.99' : '$249.99';
+ basic_plan_price.textContent = basic_plan === '$199.99' ? '$19.99' : '$199.99';
+
+})
+
+// let basicPlanPrice = document.getElementById('basic-price-plan');
+// let masterPlanPrice = document.getElementById('master-price-plan');
+// let professionalPlanPrice = document.getElementById('professional-price-plan');
+// let checkbox = document.getElementById('checkbox');
+
+// checkbox.addEventListener('click' , function(){
+//   let basicPlan = basicPlanPrice.textContent;
+//   let masterPlan = basicPlanPrice.textContent;
+//   let professionalPlan = basicPlanPrice.textContent;
+//   if(basicPlanPrice.textContent === '$399.99'){
+//     return basicPlanPrice.textContent = '$39.99'
+//   } else{
+//     return basicPlanPrice = '$399.99';
+//   }
+// })
+

0104504124f1ab9205ed92e08872ef21c281cfa4
yassmittal <yashmittalmm@gmail.com>
2022-12-07 11:43:56 +0530

    almost completed  script

--- exercises/musicPlayer/script.js
+++ exercises/musicPlayer/script.js
@@ -1,20 +1,33 @@
 let playButton = document.querySelector('.play-button');
 let prevButton = document.querySelector('.prev-button');
-let sound1 = document.querySelector('.audio audio');
-let image = document.querySelector('.image img')
+let nextButton = document.querySelector('.next-button')
+let sounds = document.querySelectorAll('.audio audio');
+let images = document.querySelectorAll('.images img')
 
-playButton.addEventListener('click', startOrEndPlay)
+let index = 0;
+
+
+playButton.addEventListener('click', startOrEndPlay);
+nextButton.addEventListener('click' , nextSound)
+prevButton.addEventListener('click' , prevSound)
+
+// playButton.addEventListener('click', startOrEndPlay)
 
 function startOrEndPlay(){
 
   if(playButton.classList.contains('notplaying')){
-    playSound(sound1)
+    playSound(sounds[index])
     playButton.setAttribute('class' , 'playing')
-    image.classList.add('rotate')
+    playButton.firstElementChild.setAttribute('class' , 'fas fa-pause play-button-icon')
+    images[index].classList.add('rotate')
+
   }else if(playButton.classList.contains('playing')){
-    stopSound(sound1)
+    stopSound(sounds[index])
+    hideAllOtherImages()
     playButton.setAttribute('class' , 'notplaying')
-    image.classList.remove('rotate')
+    images[index].classList.add('show-image')
+    images[index].classList.remove('rotate')
+    playButton.firstElementChild.setAttribute('class' , 'fas fa-play pause-button-icon')
   }
 
 }
@@ -26,3 +39,41 @@ function playSound(sound){
 function stopSound(sound){
   sound.pause();
 }
+
+function nextSound(){
+  ++index;
+  if(index > sounds.length-1){
+    index = 0;
+  }
+  hideAllOtherImages()
+  stopAllSound()
+  playButton.firstElementChild.setAttribute('class' , 'fas fa-play pause-button-icon')
+}
+
+
+function prevSound(){
+  --index;
+  if(index < 0){
+    index = sounds.length - 1;
+  }
+  hideAllOtherImages()
+  stopAllSound()
+  playButton.firstElementChild.setAttribute('class' , 'fas fa-play pause-button-icon')
+}
+
+function hideAllOtherImages(){
+  for(let i = 0; i < images.length; i++){
+    images[i].setAttribute("class", "hide-image")
+    images[index].classList.add('show-image')
+  }
+}
+
+function stopAllSound(){
+  for(let i = 0; i < sounds.length; i++){
+    stopSound(sounds[i])
+  }
+}
+
+console.log(images[0])
+
+console.log(images[1])

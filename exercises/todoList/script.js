af20a306bf3fdd1143f043d8b8307d3208f05c2c
yassmittal <yashmittalmm@gmail.com>
2022-12-09 09:10:32 +0530

    adding functionality to todo list

---
+++ exercises/todoList/script.js
@@ -0,0 +1,33 @@
+let inputForToDo = document.querySelector('#todo-text');
+let toDoList = document.querySelector('.todo-list');
+
+inputForToDo.addEventListener('keypress' , (event) => {
+  
+  if(event.key === 'Enter'){
+    event.preventDefault();
+    if(event.target.value){
+      let li = document.createElement('li');
+      li.classList.add('todo-element');
+      toDoList.appendChild(li);
+      li.textContent = event.target.value;
+      inputForToDo.value = '';
+      li.addEventListener('contextmenu' , (e)=>{
+        e.preventDefault()
+        li.remove()
+      })
+      
+      li.addEventListener('click' , (e) =>{
+        e.preventDefault()
+        li.classList.toggle('completed')
+      })
+    }
+
+    
+  }
+})
+
+// for(let i = 0; i < lists.length; i++){
+//   lists[i].addEventListener('click' , ()=>{
+//     console.log('cliedk')
+//   })
+// }

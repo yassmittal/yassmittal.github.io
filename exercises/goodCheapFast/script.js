92b7c37b741c80ed13ff1e307e4c4b9769dae578
yassmittal <yashmittalmm@gmail.com>
2022-12-16 14:31:23 +0530

    adding funtionality to goodCheapFast Project

--- exercises/goodCheapFast/script.js
+++ exercises/goodCheapFast/script.js
@@ -1,7 +1,80 @@
-let toggles = document.querySelectorAll('.toggle-input')
+// let goodToggle = document.querySelector('#good-toggle')
+// let cheapToggle = document.querySelector('#cheap-toggle')
+// let fastToggle = document.querySelector('#fast-toggle');
+let allToggles = [...document.querySelectorAll('input')]
 
-for(let i = 0; i < toggles.length; i++){
-  if(toggles[i].checked === false){
-    toggles[i] = false;
+
+
+
+
+for(let i = 0; i < allToggles.length; i++){
+  allToggles[i].addEventListener('change', (e)=>{
+    e.preventDefault()
+  let isAllTrue = allToggles.every(item => {
+    return item.checked ===  true;
+  })
+
+  if(isAllTrue === true){
+    if(e.target.id === 'good-toggle'){
+      allToggles[2].checked = false;
+    }else if(e.target.id === 'cheap-toggle'){
+      allToggles[0].checked = false;
+    }else if(e.target.id === 'fast-toggle'){
+      allToggles[1].checked = false;
+    }
   }
+
+  } )
 }
+
+
+
+
+
+// for(let i = 0; i < allToggles.length; i++){
+//   if(allToggles[i].checked === true){
+//     console.log('true');
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i = 0; i < allToggles.length; i++){
+//   allToggles[i].addEventListener('click', ()=>{
+//     if(goodToggle.checked && cheapToggle.checked && fastToggle.checked){
+//       if(i = 0){
+//         allToggles[2].checked = false;
+//       }else if(i = 1){
+//         allToggles[0].checked = false;
+//       }else if(i = 2){
+//         allToggles[1].checked = false;
+//       }
+//     }
+
+//   })
+// }
+
+
+
+
+
+// console.log(goodToggle.checked);
+// console.log(cheapToggle.checked);
+// console.log(fastToggle.checked);
+// console.log(allToggles);
+
+

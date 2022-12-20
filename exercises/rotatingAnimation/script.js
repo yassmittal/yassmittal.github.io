7eb43ede67eb49a320bdca53e8b31117625678eb
yassmittal <yashmittalmm@gmail.com>
2022-12-20 09:51:19 +0530

    finalising project by debuggin completely

--- exercises/rotatingAnimation/script.js
+++ exercises/rotatingAnimation/script.js
@@ -2,10 +2,11 @@ let hamburgur = document.querySelector('.hamburger')
 let cross = document.querySelector('.cross')
 let mainSection = document.querySelector('main');
 let sideNavBar = document.querySelector('.side-navbar-wrapper')
+let sideBar = document.querySelector('.side-bar')
 
-  hamburgur.addEventListener('click' , ()=>{
+  sideBar.addEventListener('click' , ()=>{
     mainSection.classList.toggle('rotate');
-    sideNavBar.classList.toggle('hide');
     hamburgur.classList.toggle('hide');
     cross.classList.toggle('hide');
+    sideNavBar.classList.toggle('hide');
   })

1347273254aad187b277a555b210242b0b2652f3
Yash Mittal <yashmittalmm@gmail.com>
2024-01-14 02:07:21 +0530

    added previous next level and instruction modal in the memory matrix game

--- JavascriptProjects/MemoryMatrix/script.js
+++ JavascriptProjects/MemoryMatrix/script.js
@@ -2,18 +2,36 @@ const allDOMTiles = [...document.querySelectorAll(".game-container > div")];
 const TimerWrapper = document.querySelector(".TimerWrapper");
 const TimerContent = TimerWrapper.querySelector(".TimerContent");
 const result = document.querySelector(".result");
+const levelEl = document.querySelector('[data-id="current-level"]');
+const startBtn = document.querySelector('[data-id="start-btn"]');
+const prevLevelBtn = document.querySelector('[data-id="previous-level"]');
+const nextLevelBtn = document.querySelector('[data-id="next-level"]');
+const hiddenTilesNumEl = document.querySelector(
+  '[data-id="hidden-tiles-number"]'
+);
+let modalContent = document.querySelector(".modal-content");
+const questionBtn = document.querySelector(".question-btn");
+const modalWrapper = document.querySelector(".modalWrapper");
 
-const StartBtn = document.querySelector(".start-btn");
-const allJSTiles = [];
+let currentLevel = 1;
+let totalTiles = 16;
+let NumOfTiles = currentLevel + 3;
+let allJSTiles = [];
 let randomNumArr = [];
 let isPlaying = false;
 let clickedTiles = 0;
+let arrToRender;
+renderCurrentLevel();
+
+function calculateLevelAndtiles() {
+  NumOfTiles = currentLevel + 3;
+}
 
 function generateRandomUniqueNumsArr(num) {
   let randomNum;
 
   function generateRandomNum() {
-    randomNum = Math.floor(Math.random() * 16);
+    randomNum = Math.floor(Math.random() * totalTiles);
     if (randomNumArr.includes(randomNum)) {
       generateRandomNum();
     }
@@ -26,7 +44,7 @@ function generateRandomUniqueNumsArr(num) {
 }
 
 function showHiddenTiles() {
-  let randomNumArr = generateRandomUniqueNumsArr(8);
+  let randomNumArr = generateRandomUniqueNumsArr(NumOfTiles);
   renderDOMTiles(randomNumArr);
 }
 
@@ -41,8 +59,14 @@ function renderDOMTiles(tilePositionArr) {
 }
 
 function startGame() {
-  isPlaying = true;
+  allJSTiles = [];
+  randomNumArr = [];
+  clickedTiles = 0;
+  clearDomTiles();
+  arrToRender = [];
   showTimer();
+  result.textContent = "";
+  isPlaying = false;
 }
 
 function showTimer() {
@@ -54,19 +78,45 @@ function showTimer() {
     if (pastTime == 0) {
       TimerWrapper.classList.add("hide");
       clearInterval(timerInterval);
-      let arrToRender = generateRandomUniqueNumsArr(8);
+      arrToRender = generateRandomUniqueNumsArr(NumOfTiles);
       renderDOMTiles(arrToRender);
-      runClearDomTimer(arrToRender);
+      startBtn.textContent = "Restart The Game";
+      runClearDomTimer();
     }
 
     --pastTime;
   }, 1000);
 }
-StartBtn.addEventListener("click", (e) => {
+startBtn.addEventListener("click", (e) => {
   e.preventDefault;
   startGame();
 });
 
+prevLevelBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  goToPrevLevel();
+});
+
+nextLevelBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  goToNextLevel();
+});
+
+questionBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalWrapper.classList.add("visible");
+  modalContent.classList.add("visible");
+});
+
+modalContent.addEventListener("click", (e) => {
+  e.stopImmediatePropagation();
+});
+modalWrapper.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  modalWrapper.classList.remove("visible");
+  modalContent.classList.remove("visible");
+});
 allDOMTiles.forEach((DOMTile) => {
   DOMTile.addEventListener("click", (e) => {
     if (isPlaying == false) return;
@@ -83,7 +133,7 @@ allDOMTiles.forEach((DOMTile) => {
     } else {
       DOMTile.classList.add("rotated");
     }
-    if (clickedTiles >= 8) {
+    if (clickedTiles >= NumOfTiles) {
       showResult();
       isPlaying = false;
       return;
@@ -91,18 +141,37 @@ allDOMTiles.forEach((DOMTile) => {
   });
 });
 
-function runClearDomTimer(arrToRender) {
+function runClearDomTimer() {
   setTimeout(() => {
-    clearDomTiles(arrToRender);
+    clearDomTiles();
+    isPlaying = true;
   }, 3000);
 }
 
-function clearDomTiles(tilePositionArr) {
-  tilePositionArr.forEach((tileIndex) => {
-    allDOMTiles[tileIndex].classList.remove("rotated");
+function clearDomTiles() {
+  allDOMTiles.forEach((tile) => {
+    tile.classList.remove("rotated");
+    tile.classList.remove("wrongTile");
   });
 }
 
 function showResult() {
   result.textContent = "You finished the Game";
+}
+
+function renderCurrentLevel() {
+  calculateLevelAndtiles();
+  levelEl.textContent = currentLevel;
+  hiddenTilesNumEl.textContent = NumOfTiles;
+}
+
+function goToNextLevel() {
+  ++currentLevel;
+  renderCurrentLevel();
+  clearDomTiles();
+}
+function goToPrevLevel() {
+  --currentLevel;
+  renderCurrentLevel();
+  clearDomTiles();
 }
